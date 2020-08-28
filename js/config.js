a44397550fd8aeac9c52a908d3a9248a6ddc6ee7
Bruno Manoel <contato@brunao.net>
2020-08-28 11:25:01 -0300

    Primeira versão

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["Eu não sou o meu trabalho.", "Eu sou um ninja.", "Eu trabalho com aplicações web.", "Eu trabalho com sistemas distribuídos.", "Eu trabalho com aplicações nativas na nuvem.", "Eu desenvolvo aplicações seguras.", "Eu gosto de pudim.", "Que a força esteja com você!"];
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Front page effect - can be 'none', 'particles', 'particle-fountain', 'video'  */
+var config_frontPageEffect = 'particles';
